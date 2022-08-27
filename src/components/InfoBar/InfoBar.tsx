4a76420fd515d050147d3a63de1d05a09e0f4483
17o3 <17o3gutk29vinqb@gmail.com>
2022-08-27 16:30:01 +0800

    infobar button

--- src/components/InfoBar/InfoBar.tsx
+++ src/components/InfoBar/InfoBar.tsx
@@ -16,9 +16,12 @@ const InfoBar = ({ room }: Props) => {
         <h3>{room}</h3>
       </div>
       <div className="rightInnerContainer">
-        <a href="/">
+        {/* <a href="/">
           <img src={closeIcon} alt="close icon" />
-        </a>
+        </a> */}
+        <button className="btn btn-circle btn-outline hover:bg-slate-400">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
+        </button>
       </div>
     </div>
   )
